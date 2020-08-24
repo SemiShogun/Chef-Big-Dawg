a8dac4b6baf6cadd33540d637a30fc3ad2e5bc54
Jamie Lam <jamie.lam@id.ethz.ch>
2020-08-24 14:57:57 +0200

    Added Quicksort

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] Sonic) {
         System.out.println("Here is an example of Quicksort.");
-
+        int[] values = {12,32,534,674,634,868,3,4,78,27,3,6,5435,34,1};
+        sort(values);
     }
 
     public static void sort(int[] values) {
@@ -13,13 +16,34 @@ public class QuickSort {
             return;
         }
         quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void quickSort(int[] arr, int low, int high) {
-
+        if(low < high) {
+            int splitPoint = partition(arr, low, high);
+            quickSort(arr, low, splitPoint);
+            quickSort(arr, splitPoint + 1, high);
+        }
     }
 
+    // Hoare partition algorithm
     private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low - 1, j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while(arr[i] < pivot);
+            do {
+                j--;
+            } while(arr[j] > pivot);
+            if(i < j) {
+                swap(arr, i, j);
+            } else {
+                return j;
+            }
+        }
 
     }
 
